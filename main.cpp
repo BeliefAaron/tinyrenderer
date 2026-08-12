8f79a6820ba8d832df54cf2c28c88687534caef3
Hezexin <729062742@qq.com>
2026-08-12 22:52:51 +0800

    lesson9: Tangent Space-use _nm_tangent as suffix normalMap,  optimize the Shading effect

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ struct PhoneShader : IShader {
     vec4 tri[3];  // triangle in eye coordinates
     vec4 eye;     // camera position in eye coordinates
     vec2 varying_uv[3];
+    vec4 varying_norm[3];
 
     PhoneShader(const vec3 &light, const Model &m, const vec3 &e) : model(m) {
         l = normalized((ModelView*vec4{light.x, light.y, light.z, 0.}));  // transform the light vector to view coordinates
@@ -24,24 +25,31 @@ struct PhoneShader : IShader {
         vec4 gl_Position = ModelView * vec4{v.x, v.y, v.z, 1.};
         tri[vert] =  gl_Position;                            // in eye coordinates
         varying_uv[vert] = model.uv(face, vert);
+        varying_norm[vert] = ModelView.invert_transpose() * model.normal(face, vert);
         return Perspective * gl_Position;                         // in clip coordinates
     }
 
     virtual std::pair<bool,TGAColor> fragment(const vec3 bar) const {
-        // vec3 n = bar[0]*vertexNorms[0] + bar[1] * vertexNorms[1] + bar[2] * vertexNorms[2]; // 法线向量
+        mat<2,4> E = {tri[1] - tri[0], tri[2] - tri[0]};
+        mat<2,2> U = {varying_uv[1] - varying_uv[0], varying_uv[2] - varying_uv[0]};
+        mat<2,4> T = U.invert() * E;
+        mat<4,4> tanBaseTran = {normalized(T[0]),
+                                normalized(T[1]),
+                                normalized(varying_norm[0] * bar[0] + varying_norm[1] * bar[1] + varying_norm[2] * bar[2]),
+                                {0, 0, 0, 1.}};
+
         vec2 uv = varying_uv[0] * bar[0] + varying_uv[1] * bar[1] + varying_uv[2] * bar[2];
-        vec4 n = normalized(ModelView.invert_transpose() * model.normal(uv));
-        
+        vec4 n = normalized(tanBaseTran.transpose() * model.normal(uv));
         vec4 r = normalized(2*n*(n*l) - l);   // 反射光向量
         vec4 fragPos = tri[0] * bar.x + tri[1] * bar.y + tri[2] * bar.z;
         
         vec4 viewDir = normalized(eye - fragPos);
 
-        double ambient = 0.3; // 环境光
-        double diffuse = std::max(0., n*l); // 漫反射
+        double ambient = 0.4; // 环境光
+        double diffuse = std::max(0., n*l); // 漫反射强度
         double specularMask = sample2D(model.specular(), uv)[0] / 255.;
         double specular = specularMask * std::pow(std::max(0., r * viewDir), 32); // 镜面反射强度，因为相机在相机坐标系中的z轴正方向，所以取r.z作为视线方向与反射光的夹角余弦值
-        double illumination = ambient + diffuse + 5*specular;
+        double illumination = ambient + diffuse + specular;
 
         TGAColor fragColor = sample2D(model.diffuse(), uv);
         
--- model.cpp
+++ model.cpp
@@ -49,7 +49,7 @@ Model::Model(const std::string filename) {
         std::string texfile = filename.substr(0,dot) + suffix;
         std::cerr << "texture file " << texfile << " loading " << (img.read_tga_file(texfile.c_str()) ? "ok" : "failed") << std::endl;
     };
-    load_texture("_nm.tga", normalMap);
+    load_texture("_nm_tangent.tga", normalMap);
     load_texture("_diffuse.tga", diffuseMap);
     load_texture("_spec.tga", specularMap);
 }
@@ -71,7 +71,7 @@ vec4 Model::normal(const int iface, const int nthvert) const {
 
 vec4 Model::normal(const vec2& uv) const {
     TGAColor c = normalMap.get(uv[0]*normalMap.width(), uv[1]*normalMap.height());
-    return vec4{(double)c[2],(double)c[1],(double)c[0],0}*2./255. - vec4{1,1,1,0}; 
+    return normalized(vec4{(double)c[2],(double)c[1],(double)c[0],0}*2./255. - vec4{1,1,1,0}); 
 }                     
 
 vec2 Model::uv(const int iface, const int nthvert) const {
