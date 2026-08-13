618e13e11bf95924ecd590b8e798ad7eaa811ff7
Hezexin <729062742@qq.com>
2026-08-13 21:32:40 +0800

    fix name mistake

--- main.cpp
+++ main.cpp
@@ -181,10 +181,10 @@ int main(int argc, char** argv) {
             vec4 fragment = MtoObj * vec4{ static_cast<double>(x), static_cast<double>(y), zbuffer_cached[x + y*height], 1.};
             vec4 fragmentInLight = N * fragment;
             vec3 p = fragmentInLight.xyz() / fragmentInLight.w; // ÒõÓ°Í¼×ø±ê
-            bool shouldMasked = p.x < 0 || p.x >= shadowW || p.y < 0 || p.y >= shadowH ||  // is out of boundary
+            bool isLit = p.x < 0 || p.x >= shadowW || p.y < 0 || p.y >= shadowH ||  // is out of boundary
                                 p.z <= -100. || // is backgroud
                                 p.z > zbuffer[(int)p.x + (int)p.y * shadowH] - 0.03;
-            mask[x + y * width] = shouldMasked;                      
+            mask[x + y * width] = isLit;                      
         }
     }
 
